7375cec1cd2b338aa9f656fe7c6da407aa002843
yuuki1218 <58942798+yuuki1218@users.noreply.github.com>
2020-10-01 10:50:16 +0900

    update Study3.js

--- Study3.js
+++ Study3.js
@@ -1,5 +1,5 @@
 //必要なエレメントを取得
-const todos = {};
+const todos = [];
 
 const btn = document.querySelector(".task-btn");
 const tbody = document.querySelector("tbody");
@@ -9,6 +9,10 @@ let idNum = 0;
 const taskCreate = function () {
   //comment
   let title = document.querySelector(".title").value;
+  if (!title) {
+    alert("入力してください。");
+    return;
+  }
 
   //status-btn
   const statusBtn = document.createElement("input");
@@ -21,24 +25,32 @@ const taskCreate = function () {
   deleteBtn.value = "削除";
 
   //todoの格納
-  todos.id = idNum++;
-  todos.comment = title;
-  todos.status = statusBtn;
-  todos.delete = deleteBtn;
+
+  const todo = {
+    comment: title,
+    status: statusBtn,
+    delete: deleteBtn,
+  };
+
+  todo.id = todos.length;
+  todos.push(todo);
+  // console.log(todos);
 
   //todoを一つずつ作る。
   const tasks = document.createElement("tr");
   tbody.appendChild(tasks);
 
   const itemId = document.createElement("td");
+  itemId.className = "id";
+  itemId.value = todo.id;
   const itemComment = document.createElement("td");
   const itemStatus = document.createElement("td");
   const itemDelete = document.createElement("td");
 
-  itemId.textContent = todos.id;
-  itemComment.textContent = todos.comment;
-  itemStatus.appendChild(todos.status);
-  itemDelete.appendChild(todos.delete);
+  itemId.textContent = todo.id;
+  itemComment.textContent = todo.comment;
+  itemStatus.appendChild(todo.status);
+  itemDelete.appendChild(todo.delete);
 
   tasks.appendChild(itemId);
   tasks.appendChild(itemComment);
@@ -46,6 +58,35 @@ const taskCreate = function () {
   tasks.appendChild(itemDelete);
 
   resetTask();
+  deleteBtn.addEventListener("click", () => removeTask(deleteBtn));
 };
 
+//form内のリセット
 let resetTask = () => (document.querySelector(".title").value = "");
+
+//削除ボタンが押されたら対象のタスクを消す。
+let removeTask = (deleteBtn) => {
+  const targetTask = deleteBtn.closest("tr");
+  const targetId = targetTask.querySelector(".id").value;
+
+  todos.forEach((todo, index) => {
+    if (todo.id === targetId) {
+      todos.splice(index, 1);
+    }
+  });
+  tbody.removeChild(targetTask);
+
+  updateId();
+};
+
+function updateId() {
+  todos.forEach((todo, index) => {
+    todo.id = index;
+  });
+  const allId = tbody.querySelectorAll(".id");
+  allId.forEach((id, index) => {
+    id.textContent = index;
+  });
+}
+
+btn.addEventListener("click", taskCreate);
