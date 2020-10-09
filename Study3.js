e818388554ebe25034ed5a12d0f9d3a10d95ca6a
yuuki1218 <58942798+yuuki1218@users.noreply.github.com>
2020-10-09 12:34:52 +0900

    add showStatus method

--- Study3.js
+++ Study3.js
@@ -3,6 +3,7 @@ const todos = [];
 
 const btn = document.getElementById('task-btn');
 const tbody = document.querySelector('tbody');
+const radioBtns = document.getElementById('radio-btn');
 
 //クリックされたら行を追加
 const taskCreate = function () {
@@ -34,6 +35,7 @@ const taskCreate = function () {
 
   const statusBtn = document.createElement('td');
   statusBtn.textContent = '作業中';
+  statusBtn.className = "status-value";
   task.appendChild(statusBtn);
 
   const deleteBtn = document.createElement('td');
@@ -41,15 +43,38 @@ const taskCreate = function () {
   task.appendChild(deleteBtn);
 
   resetTask();
+  showStatus();
   statusBtn.addEventListener('click', () => changeStatus(statusBtn));
   deleteBtn.addEventListener('click', () => removeTask(deleteBtn));
+  radioBtns.addEventListener('change', showStatus);
+
+  //radioボタンの選択によって表示を変える。
+  function showStatus() {
+    const radioStatusValue = radioBtns.status.value;
+    const statusItems = document.getElementsByClassName('status-value');
+    for (let i = 0; i < statusItems.length; i++) {
+      const statusItem = statusItems[i];
+      statusItem.parentNode.style.display = '';
+      if (radioStatusValue === '作業中' && statusItem.textContent === '完了') {
+        statusItem.parentNode.style.display = 'none';
+      } else if (
+        radioStatusValue === '完了' &&
+        statusItem.textContent === '作業中'
+      ) {
+        statusItem.parentNode.style.display = 'none';
+      } else if (radioStatusValue === 'すべて') {
+        statusItem.parentNode.style.display = '';
+      }
+    }
+
+  }
 };
 
 //form内のリセット
 const resetTask = () => (document.getElementById('title').value = '');
 
 //削除ボタンが押されたら対象のタスクを消す。
-let removeTask = (deleteBtn) => {
+const removeTask = (deleteBtn) => {
   const targetTask = deleteBtn.closest('tr');
   const targetId = targetTask.querySelector('.id').textContent;
   todos.splice(targetId, 1);
@@ -68,7 +93,6 @@ function updateId() {
 
 //状態を切り替えるメソッド
 function changeStatus(statusBtn) {
-  console.log(statusBtn.textContent);
   if (statusBtn.textContent === '作業中') {
     statusBtn.textContent = '完了';
     return;
